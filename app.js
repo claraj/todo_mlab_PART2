1d2a1a589a3dffb786ccfbc99728bc908cf53792
clara <clara@clara.com>
2018-02-09 18:10:47 -0600

    todo app

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var flash = require('express-flash');
+var session = require('express-session');
+var mongoose = require('mongoose');
+
+// Read the mLab connection URL
+var db_url = process.env.MONGO_URL;
+
+// And connect to the database. Print success or error message.
+mongoose.connect(db_url)
+  .then( () => { console.log('Connected to mLab'); })
+  .catch( (err) => { console.log('Error connecting to mLab', err); });
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(session({secret: 'top secret', resave: false, saveUninitialized: false}));
+app.use(flash());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  
+  if (err.kind === 'ObjectId' && err.name == 'CastError') {
+    err.status = 404;
+    err.message = "ObjectId Not Found";
+  }
+  
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.log(err.kind)
+  console.log(err.name)
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,143 @@
+var express = require('express');
+var router = express.Router();
+var Task = require('../models/task');
+
+/* GET home page, list of incomplete tasks */
+router.get('/', function(req, res, next) {
+  
+  Task.find( {completed: false} )
+    .then( (docs) => {
+      res.render('index', {title: 'Incomplete tasks', tasks: docs} );
+    })
+    .catch( (err) => {
+      next(err);    // Forward to the error handlers
+    });
+  
+});
+
+
+
+
+/* POST to create a new task */
+
+router.post('/add', function(req, res, next){
+  
+  // Check if something was entered in the text input
+  if (req.body.text) {
+    // Create new Task
+    var t = new Task({text: req.body.text, completed: false})
+    // Save the task, and redirect to home page if successful
+    t.save().then((newTask) => {
+      console.log('The new task created is ', newTask); // just for debugging
+      res.redirect('/');  // Creates a GET request to '/'
+    }).catch(() => {
+      next(err);   // Forward error to the error handlers
+    });
+  }
+  else {
+    console.log('enter a task eh')
+    req.flash('error', 'Please enter a task.')  // Flash an error message
+    res.redirect('/');  // And redirect to the home page
+  }
+  
+});
+
+
+/* POST to mark a task as done */
+
+router.post('/done', function(req, res, next){
+  
+  Task.findByIdAndUpdate( req.body._id, {completed: true})
+    .then( (originalTask) => {
+      // originalTask only has a value if a document with this _id was found
+      if (originalTask) {
+        req.flash('info', 'Task marked as done!');
+        res.redirect('/');  // redirect to list of tasks
+      } else {
+        var err = new Error('Task Not Found');  // report Task not found with 404 status
+        err.status = 404;
+        next(err);
+      }
+    })
+    .catch( (err) => {
+      next(err);     // to error handlers
+    });
+  
+});
+
+
+/* GET completed tasks */
+
+router.get('/completed', function(req, res, next){
+  Task.find({completed: true})
+    .then( (docs) => {
+      res.render('completed_tasks', {tasks: docs});
+    })
+    .catch( (err) => {
+      next(err);
+    })
+});
+
+
+/* POST to delete a task */
+
+router.post('/delete', function(req, res, next){
+  
+  Task.findByIdAndRemove(req.body._id)
+    .then( (deletedTask) => {
+      if (deletedTask) {
+        req.flash('info', 'Task deleted.');
+        res.redirect('/');
+      } else {
+        var error = new Error('Task Not Found')
+        error.status = 404;
+        next(error);
+      }
+  })
+    .catch( (err) => {
+      next(err);
+    })
+  
+});
+
+
+/* POST mark all tasks as done */
+
+router.post('/alldone', function(req, res, next){
+
+  Task.updateMany({completed: false}, {completed: true})
+    .then( () => {
+      req.flash('info', 'All tasks are done!');
+      res.redirect('/');  // If prefered, redirect to /completed
+    })
+    .catch( (err) => {
+      next(err);
+    });
+  
+});
+
+
+/* GET details about one task */
+
+router.get('/task/:_id', function(req, res, next){
+  
+  Task.findById(req.params._id)
+    .then( (doc) => {
+      if (doc) {
+        res.render('task', {task: doc});
+      }
+      else {
+        next();  // to the 404 error handler
+      }
+    })
+    .catch( (err) => {
+      next(err);
+    });
+  
+});
+
+
+module.exports = router;
+
+
+
